0cbc8973a4646c5b21521017bcf12226bb685d2a
DyogoViana <dyogo.vianaa@gmail.com>
2023-06-09 16:01:26 -0300

    apagar para recomeçar.

---
+++ js/parallax.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let elems = document.querySelectorAll('.parallax');
+    // var instances = M.Parallax.init(elems, options);
+    console.log('teste');
+});
